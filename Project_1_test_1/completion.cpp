5161414a45ef92044e72566a1c946b6afbca06e0
Jack Scrivener <10jack222@gmail.com>
2024-02-28 09:01:09 +1300

    working sin wave

--- Project_1_test_1/completion.cpp
+++ Project_1_test_1/completion.cpp
@@ -17,25 +17,26 @@ using namespace::std;
 
 //------- Main ---------
 int main() {
-    int sample_rate = 100; // samples per second, select value which provides good quality sound  
+    //44100 is cd quality
+    int sample_rate = 300; // samples per second, select value which provides good quality sound  
     double duration = 10; // how long [seconds] it will sound 
     double n_samples = duration * sample_rate; // if sound is "duration" seconds long and there are "sample_rate" samples per second 
     // - how many samples are there altogether? What type should this variable be? 
 
-    //... dt = ...; // time between samples 
+    float dt = 1.0/sample_rate; // time between samples 
     //int* waveform = new int[n_samples]; // creates the array 
 
     // or you can use vector 
     std::vector<int> waveform;
-    int frequency = 24000;// pitch of the sound 
+    float frequency = 1;// pitch of the sound 
     int volume = 30000;// 6000 is loud enough 
 
     for (int i_sample = 0; i_sample < n_samples; i_sample++) {
         cout << "test text";
         // if using vector 
-        waveform.push_back(frequency);
+        waveform.push_back(static_cast<int>(volume*sin(2*3.14159*frequency*i_sample*dt)));
         //cout an be used here to check values of "waveform" 
-        cout << frequency;
+        cout << static_cast<int>(volume * sin(2 * 3.14159 * frequency * i_sample * dt));
     }
     // generates sound file from waveform array, writes n_samples numbers  
     // into sound wav file 
